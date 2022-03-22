75794e269911fb2d3af02dad7fb9db0ce91a81f8
nkilicarslan <necatkilicarslann@gmail.com>
2022-03-23 01:36:52 +0300

    Solved

---
+++ Max_array_sum.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maxSubsetSum function below.
+int maxSubsetSum(vector<int> arr) {
+    int arr_size = arr.size();
+    int max_val = 0;
+    if(arr_size == 1){
+        if(arr[0] < 0) return 0;
+        else return arr[0];
+    }
+    else if(arr_size == 2){
+        max_val = max(arr[0],arr[1]);
+        if(max_val<0) return 0;
+        else return max_val;
+    }
+    else{
+        arr[1] = max(arr[1],arr[0]);
+        for (int i = 2; i < arr_size; ++i) {
+            arr[i] = max(max(arr[i] + arr[i-2], arr[i]),arr[i-1]);
+        }
+    }
+
+    return arr[arr_size-1];
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = maxSubsetSum(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
